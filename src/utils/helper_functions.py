eb0b7e82360a77657eb6e704f1ae7f0ad3d348f5
CreepyMemes <61922260+CreepyMemes@users.noreply.github.com>
2023-01-23 20:53:35 +0100

    Refactored code to keep only general functions

---
+++ src/utils/helper_functions.py
@@ -0,0 +1,21 @@
+# Check if a given string is a valid integer or float
+def isnumber(num):
+    # Check if the number is an integer with more than 3 digits
+    if num.find('.') == -1 and len(num) > 3:
+        return False
+    # Check if the number is a valid float with more than 3 digits
+    if num.find('.') > -1 and len(num.split('.')[0]) > 3:
+        return False
+    # Check if the number is a valid float with more than 2 decimals
+    if num.find('.') > -1 and len(num.split('.')[1]) > 2:
+        return False
+    # Check if the number is an empty string
+    if len(num) == 0:
+        return False
+    # Check if the number is a valid float with only one '.'
+    if num.replace('.','',1).isdigit() and num.count('.') < 2:
+        return True
+
+# Check if a string is empty
+def is_empty(value):
+    return len(value) == 0 
